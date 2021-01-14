30fd54ddbecaf4612b0dd7ec5aa57c20eb8be4c1
Kevin <kevhines@gmail.com>
2021-01-13 21:21:32 -0500

    commit tree 062a3a9ce7948dd09922d178d04afd1e3408bb3c

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require "pry"
 def key_for_min_value(name_hash)
-
+    min_price = nil
+    min_item = nil
+    name_hash.each do |item, price|
+       if min_price
+            if price <= min_price 
+                min_price = price
+                min_item = item
+                #binding.pry
+            end 
+        else
+            min_price = price
+            min_item = item
+        end
+    end
+    min_item
 end
